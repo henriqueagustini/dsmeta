2d1cd3a68cb27d32d5787a273917e48a40f65b83
Henrique Agustini <henrique80@gmail.com>
2022-07-15 21:44:42 -0300

    react-hot-toast

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,12 +1,11 @@
-import { ToastContainer } from "react-toastify";
-import "react-toastify/dist/ReactToastify.css";
+import { Toaster } from "react-hot-toast";
 import Header from "./components/Header";
 import SalesCard from "./components/SalesCard";
 
 function App() {
   return (
     <>
-      <ToastContainer />
+      <Toaster />
       <Header />
       <main>
         <section id="sales">
--- frontend/src/components/NotificationButton/index.tsx
+++ frontend/src/components/NotificationButton/index.tsx
@@ -1,5 +1,5 @@
 import axios from "axios";
-import { toast } from "react-toastify";
+import toast from "react-hot-toast";
 import icon from "../../assets/img/notification-icon.svg";
 import { BASE_URL } from "../../utils/request";
 
@@ -11,7 +11,7 @@ type Props = {
 
 function handleClick(id: number) {
   axios(`${BASE_URL}/sales/${id}/notification`).then(response => {
-    toast.info("SMS enviado com sucesso");
+    toast("SMS enviado com sucesso");
   });
 }
 
